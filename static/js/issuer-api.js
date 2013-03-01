321633d064f1876d6578207bcda43c8d2a5c2cfe
Mike Larsson <mikelarssonftw@gmail.com>
2013-03-01 14:50:21 -0500

    Moving issue method to select menu

--- static/js/issuer-api.js
+++ static/js/issuer-api.js
@@ -5,16 +5,44 @@ define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
 
     var script = $('#issuer-api');
 
+    self.availableMethods = [];
+
+    function setMethods() {
+      if( OpenBadges.issue ) { 
+        self.availableMethods.push({ key: 'modal', name: 'modal Issuer API' }); 
+      }
+      if( OpenBadges.issue_no_modal ) {
+        self.availableMethods.push({ key: 'modaless', name: 'modaless Issuer API' }); 
+      }
+      if( OpenBadges.connect ) {
+        self.availableMethods.push({ key: 'connect', name: 'Backpack Connect' }); 
+      }
+    }
+
     self.reloadFrom = function(server) {
+      self.availableMethods = [];
       self.trigger('reload', server.get('name'));
+
       script.attr('src', server.get('url'));
       return $.getScript(server.get('url'))
         .success(function(){
+          setMethods();
           self.trigger('success');
         })
         .fail(function(){
           self.trigger('error');
         });
+    };
+  
+    self.issue = function(method, assertions) {
+      switch(method) {
+        case 'modal':
+          OpenBadges.issue(assertions);
+          break;
+        case 'modaless':
+          OpenBadges.issue_no_modal(assertions);
+          break;
+      }
     };
 
     _.extend(self, Backbone.Events);
