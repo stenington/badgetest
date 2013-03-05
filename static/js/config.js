b520e8981e6b76b919d9f677e572fb8b7158033c
Mike Larsson <mikelarssonftw@gmail.com>
2013-03-05 18:25:34 -0500

    add mofostaging too

--- static/js/config.js
+++ static/js/config.js
@@ -4,8 +4,9 @@ define({
 
   defaultServers: [
     { name: 'development', url: 'http://dev.openbadges.org/' },
+    { name: 'mofodev staging', url: 'http://openbadgesdev.mofostaging.net' },
     { name: 'production', url: 'http://beta.openbadges.org/' },
-    { name: 'stenington', url: 'http://stenington.org:8889' }
+    { name: 'stenington', url: 'http://stenington.org:8889' },
   ]
     
 });
