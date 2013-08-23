2d86ff2c06e69c7e3a47386299b3da73ec365e0a
Mike Larsson <mikelarssonftw@gmail.com>
2013-08-23 14:43:38 -0400

    Add error handling with test

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,22 @@
+var should = require('should');
+var sinon = require('sinon');
+
 var request = require('./lib/util').request;
 
-describe("app", function() {
-  it("returns 'HELLO WORLD' at /", function(done) {
-    request()
-      .get('/')
-      .expect(200)
-      .expect('HELLO WORLD')
-      .end(done);
+describe('app', function() {
+  it('reports errors', function(done) {
+    request({testRoutes: {
+      'GET /forced-error': function(req, res, next) {
+        sinon.stub(process.stderr, 'write');
+        next(new Error('omg kaboom'));
+      }
+    }})
+      .get('/forced-error')
+      .expect('Sorry, something exploded!')
+      .expect(500, function(err) {
+        process.stderr.write.calledWithMatch('omg kaboom').should.eql(true);
+        process.stderr.write.restore();
+        done(err);
+      });
   });
 });
--- test/lib/util.js
+++ test/lib/util.js
@@ -8,6 +8,21 @@ exports.app = function(options) {
     cookieSecret: 's3cret'
   });
 
+  if (options.testRoutes) {
+    var testRoutes = options.testRoutes;
+
+    options.defineExtraRoutes = function(app) {
+      Object.keys(testRoutes).forEach(function(route) {
+        var parts = route.split(' ', 2);
+        var method = parts[0].toLowerCase();
+        var path = parts[1];
+
+        return app[method](path, testRoutes[route]);
+      });
+    };
+    delete options.testRoutes;
+  }
+
   return badgetest.app.build(options);
 };
 
