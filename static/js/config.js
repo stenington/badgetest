fc52045bcb5fc6ff5cf35c1fe9cd5cbd446a46e9
Mike Larsson <mikelarssonftw@gmail.com>
2013-09-25 16:29:13 -0400

    Update config to relevant environments, use protocol-less urls

--- static/js/config.js
+++ static/js/config.js
@@ -3,9 +3,8 @@ define({
   defaultBadgeImageSize: 220,
 
   defaultServers: [
-    { name: 'development', url: 'http://dev.openbadges.org/' },
-    { name: 'production', url: 'http://backpack.openbadges.org/' },
-    { name: 'mofostaging', url: 'http://openbadgesdev.mofostaging.net' },
-    { name: 'stenington', url: 'http://stenington.org:8889' }
+    { name: 'production', url: '//backpack.openbadges.org/' },
+    { name: 'staging', url: '//openbadges.mofostaging.net' },
+    { name: 'stenington', url: '//stenington.org:8889' }
   ]
 });
